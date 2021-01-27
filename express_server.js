cd02b5701f24249c9069c829f3f3d892d25de6f3
Jeremy Holman <jholman12@bcit.ca>
2021-01-27 01:08:16 -0800

    and let us start over

---
+++ express_server.js
@@ -0,0 +1,23 @@
+"use strict";
+
+require('dotenv').config();
+const PORT = process.env.PORT || 8080;
+const ENV = process.env.ENV || "development";
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
+
+app.get("/", (req, res) => {
+  res.render("index");
+})
+
+
+app.listen(PORT, () => console.log(`app up on port ${PORT}`));
